83ee584b1913f851cedb3f62f8ff9657042a5ae1
Andrey Kolishchak <andrey.kolishchak@gmail.com>
2017-04-23 15:56:12 -0400

    adding variables, labels, depths, variable based rewards, and support for imitation

--- doom_env.py
+++ doom_env.py
@@ -3,14 +3,19 @@
 #
 # Created by Andrey Kolishchak on 01/21/17.
 #
+import numpy as np
 from doom_instance import DoomInstance
 
 
 def init_doom_env(args):
-    doom = DoomInstance(args.vizdoom_config, args.wad_path, 1)
+    if args.action_set is not None:
+        args.action_set = np.load(args.action_set).tolist()
+
+    doom = DoomInstance(args.vizdoom_config, args.wad_path, args.skiprate, actions=args.action_set)
     state = doom.get_state()
 
     args.button_num = doom.get_button_num()
     args.screen_size = state.screen_buffer.shape
+    #args.screen_size = (1, state.screen_buffer.shape[1], state.screen_buffer.shape[2])
     if state.game_variables is not None:
         args.variables_size = state.game_variables.shape
--- doom_instance.py
+++ doom_instance.py
@@ -5,29 +5,40 @@
 #
 from vizdoom import *
 import numpy as np
+import datetime
+
 
 class NormalizedState:
-    def __init__(self, screen, depth, labels, variables):
+    def __init__(self, screen, variables=None, depth=None, labels=None, automap=None):
         self.screen = screen
         self.depth = depth
         self.labels = labels
         self.variables = variables
+        self.labels = labels
+        self.automap = automap
 
 
 class DoomInstance:
-    def __init__(self, config, wad, skiprate, id=None, visible=False):
+    def __init__(self, config, wad, skiprate, id=None, visible=False, actions=None):
         self.game = DoomGame()
         self.game.set_doom_game_path(wad)
         self.game.load_config(config)
-        if visible:
+        self.visible = visible
+        if self.visible:
             self.game.set_window_visible(True)
             self.game.set_sound_enabled(True)
             self.game.set_mode(Mode.ASYNC_PLAYER)
-        self.game.init()
 
-        self.game.new_episode()
-        self.button_num = len(self.game.get_available_buttons())
-        self.actions = np.eye(self.button_num, dtype=int).tolist()
+        self.variables = None
+        self.game.init()
+        self.new_episode()
+
+        if actions is None:
+            self.actions = np.eye(len(self.game.get_available_buttons()), dtype=int).tolist()
+        else:
+            self.actions = actions
+        self.button_num = len(self.actions)
+
         self.episode_return = 0
         self.skiprate = skiprate
         self.id = id
@@ -37,32 +48,56 @@ class DoomInstance:
         finished = self.game.is_episode_finished()
         if finished:
             self.episode_return = self.game.get_total_reward()
-            self.game.new_episode()
+            self.new_episode()
 
         if self.game.is_player_dead():
-            self.game.respawn_player()
+            self.new_episode(is_finished=False)
 
         state = self.game.get_state()
         return state, reward, finished
 
+    def advance(self):
+        self.game.advance_action()
+        action = self.game.get_last_action()
+        reward = self.game.get_last_reward()
+        finished = self.game.is_episode_finished()
+        return action, reward, finished
+
     def step_normalized(self, action):
         state, reward, finished = self.step(action)
-        return self.normalize(state), reward, finished
+        state = self.normalize(state)
+        if state.variables is not None:
+            reward += (state.variables - self.variables).sum()
 
-    def normalize(self, state):
-        screen = (state.screen_buffer.astype(np.float32) - 127) / 255
+        return state, reward, finished
 
-        if state.depth_buffer is not None:
-            depth = state.depth_buffer.astype(np.float32) / 255
-        else:
-            depth = None
+    @staticmethod
+    def normalize(state):
+        screen = state.screen_buffer.astype(np.float32) / 127.5 - 1.
+        #screen = state.labels_buffer / 127.5 - 1.
+        screen = screen[None, :, :]
 
         if state.game_variables is not None:
-            variables = state.game_variables.astype(np.float32)
+            variables = state.game_variables
         else:
             variables = None
 
-        return NormalizedState(screen=screen, depth=depth, labels=None, variables=variables)
+        if state.depth_buffer is not None:
+            depth = state.depth_buffer / 127.5 - 1.
+        else:
+            depth = None
+
+        if state.labels_buffer is not None:
+            labels = state.labels_buffer
+        else:
+            labels = None
+
+        if state.automap_buffer is not None:
+            automap = state.automap_buffer
+        else:
+            automap = None
+
+        return NormalizedState(screen=screen, variables=variables, depth=depth, labels=labels, automap=automap)
 
     def get_state(self):
         state = self.game.get_state()
@@ -70,7 +105,26 @@ class DoomInstance:
 
     def get_state_normalized(self):
         state = self.get_state()
-        return self.normalize(state)
+        if state is not None:
+            return self.normalize(state)
+        else:
+            return None
+
+    def is_finished(self):
+        self.game.is_episode_finished()
+
+    def new_episode(self, is_finished=True):
+        if is_finished:
+            if self.visible:
+                file_name = '{:%Y-%m-%d_%H-%M-%S}_rec.lmp'.format(datetime.datetime.now())
+                self.game.new_episode(file_name)
+            else:
+                self.game.new_episode()
+        else:
+            self.game.respawn_player()
+        state = self.game.get_state()
+        if state.game_variables is not None:
+            self.variables = state.game_variables
 
     def release(self):
         self.game.close()
