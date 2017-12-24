fixing softmax

src/aac.py
@@ -7,8 +7,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from cuda import *
 from collections import namedtuple
-from egreedy import EGreedy
 from aac_base import AACBase
+import random
 
 
 class BaseModel(AACBase):
@@ -29,6 +29,7 @@ class BaseModel(AACBase):
         self.batch_norm = nn.BatchNorm1d(self.screen_feature_num)
 
         layer1_size = 128
+        variable_num = 0
         self.action1 = nn.Linear(self.screen_feature_num, layer1_size)
         self.action2 = nn.Linear(layer1_size + variable_num, button_num)
 
@@ -41,22 +42,22 @@ class BaseModel(AACBase):
 
     def forward(self, screen, variables):
         # cnn
-        screen_features = F.relu(self.conv1(screen))
-        screen_features = F.relu(self.conv2(screen_features))
-        screen_features = F.relu(self.conv3(screen_features))
-        screen_features = F.relu(self.conv4(screen_features))
-        screen_features = F.relu(self.conv5(screen_features))
-        screen_features = F.relu(self.conv6(screen_features))
+        screen_features = F.selu(self.conv1(screen))
+        screen_features = F.selu(self.conv2(screen_features))
+        screen_features = F.selu(self.conv3(screen_features))
+        screen_features = F.selu(self.conv4(screen_features))
+        screen_features = F.selu(self.conv5(screen_features))
+        screen_features = F.selu(self.conv6(screen_features))
         screen_features = screen_features.view(screen_features.size(0), -1)
 
         # features
         input = self.screen_features1(screen_features)
         input = self.batch_norm(input)
-        input = F.relu(input)
+        input = F.selu(input)
 
         # action
-        action = F.relu(self.action1(input))
-        action = torch.cat([action, variables], 1)
+        action = F.selu(self.action1(input))
+        #action = torch.cat([action, variables], 1)
         action = self.action2(action)
 
         return action, input
@@ -110,23 +111,29 @@ class AdvantageActorCritic(BaseModel):
         self.discounts = []
 
     def forward(self, screen, variables):
-        action, input = super(AdvantageActorCritic, self).forward(screen, variables)
+        action_prob, input = super(AdvantageActorCritic, self).forward(screen, variables)
 
-        action = F.softmax(action, dim=1)
         if not self.training:
-            _, action = action.max(1, keepdim=True)
+            _, action = action_prob.max(1, keepdim=True)
             return action, None
 
-        distribution = EGreedy(action, 0.1)
-        action = distribution.sample()
+        # greedy actions
+        if random.random() < 0.1:
+            action = torch.LongTensor(action_prob.size(0), 1).random_(0, action_prob.size(1))
+            action = Variable(action)
+            if USE_CUDA:
+                action = action.cuda()
+        else:
+           _, action = action_prob.max(1, keepdim=True)
 
         # value prediction - critic
-        value = F.relu(self.value1(input))
-        value = torch.cat([value, variables], 1)
+        value = F.selu(self.value1(input))
+        #value = torch.cat([value, variables], 1)
         value = self.value2(value)
 
         # save output for backpro
-        self.outputs.append(ModelOutput(distribution.log_prob(action), value))
+        action_prob = F.log_softmax(action_prob, dim=1)
+        self.outputs.append(ModelOutput(action_prob.gather(-1, action), value))
         return action, value
 
     def get_action(self, state):
@@ -155,13 +162,19 @@ class AdvantageActorCritic(BaseModel):
             returns = returns.cuda()
         #
         # calculate losses
-        loss = 0
-        for i in range(len(self.outputs) - 1):
+        policy_loss = 0
+        value_loss = 0
+        steps = len(self.outputs) - 1
+        for i in range(steps):
             advantage = Variable(returns[i] - self.outputs[i].value.data)
-            loss += (-self.outputs[i].log_action * advantage).sum()
-            loss += F.smooth_l1_loss(self.outputs[i].value, Variable(returns[i]))
-        #
-        # backpro
+            policy_loss += -self.outputs[i].log_action * advantage
+            value_loss += F.smooth_l1_loss(self.outputs[i].value, Variable(returns[i]))
+
+        weights_l2 = 0
+        for param in self.parameters():
+            weights_l2 += param.norm(2)
+
+        loss = policy_loss.mean()/steps + value_loss/steps + 0.00001*weights_l2
         loss.backward()
 
         # reset state

src/aac_base.py
@@ -11,6 +11,11 @@ import torch.optim as optim
 from doom_instance import *
 from cuda import *
 from model import Model
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+import numpy as np
+import math
+
 
 class AACBase(Model):
     def __init__(self):
@@ -73,11 +78,27 @@ class AACBase(Model):
 
             # update model
             self.backward()
+            """
+            grads = []
+            weights = []
+            for p in self.parameters():
+                grads.append(p.grad.data.view(-1))
+                weights.append(p.data.view(-1))
+            grads = torch.cat(grads, 0)
+            weights = torch.cat(weights, 0)
+            grads_norm = grads.norm()
+            weights_norm = weights.norm()
+
+            assert grads_norm == grads_norm
+            """
+            weights_norm = 0
+            grads_norm = 0
+
             optimizer.step()
             optimizer.zero_grad()
 
             if episode % 1 == 0:
-                print("{}: mean_return = {:f}, batch_time = {:.3f}".format(episode, episode_return.mean(), time.time()-batch_time))
+                print("{}: mean_return = {:f}, grads_norm = {:f}, weights_norm = {:f}, batch_time = {:.3f}".format(episode, episode_return.mean(), grads_norm, weights_norm, time.time()-batch_time))
 
             if episode % args.checkpoint_rate == 0:
                 torch.save(self.state_dict(), args.checkpoint_file)
@@ -107,11 +128,33 @@ class AACBase(Model):
             state.variables[0, :] = torch.from_numpy(step_state.variables)
             # compute an action
             action = self.get_action(state)
+            print(action[0][0])
             # render
             step_state, _, finished = game.step_normalized(action[0][0])
             #img = step_state.automap.transpose(1, 2, 0)
-            #img = step_state.labels
-            #plt.imsave('map.jpeg', img)
+            #plt.imsave('map.png', img)
+            '''
+            img = step_state.depth
+            plt.imsave('depth.png', img, cmap=cm.gray)
+            plan = np.ndarray(shape=(240, 320, 3), dtype=float)
+            plan[:] = 0
+            for i in range(320):
+                angle = math.radians(135.0-i*90.0/320.0)
+                distance = (step_state.screen[0, 120:140, i].min() + 1)*127.5
+                x = int(160 + math.cos(angle)*distance)
+                y = int(219 - math.fabs(math.sin(angle)*distance))
+                if x >= 0 and x < 320 and y >= 0 and y < 240:
+                    channel = 0
+                    if step_state.screen[2, 120:140, i].max() == 1:
+                        channel = 2
+                    elif step_state.screen[3, 120:140, i].max() == 1:
+                        channel = 1
+                    plan[y, x, channel] = 1
+            plt.imsave('depth-plan.png', plan)
+            '''
+            img = step_state.screen[0:3, :]
+            img = img.transpose(1, 2, 0)
+            plt.imsave('depth-plan.png', img)
             if finished:
                 print("episode return: {}".format(game.get_episode_return()))
                 self.set_terminal(torch.zeros(1))

src/aac_lstm.py
@@ -8,8 +8,8 @@ import torch.nn.functional as F
 from cuda import *
 from collections import namedtuple
 from lstm import LSTM
-from egreedy import EGreedy
 from aac_base import AACBase
+import random
 
 
 ModelOutput = namedtuple('ModelOutput', ['log_action', 'value'])
@@ -95,16 +95,20 @@ class AdvantageActorCriticLSTM(BaseModelLSTM):
         self.discounts = []
 
     def forward(self, screen, variables):
-        action = super(AdvantageActorCriticLSTM, self).forward(screen, variables)
+        action_prob = super(AdvantageActorCriticLSTM, self).forward(screen, variables)
 
-        # action
-        action = F.softmax(action, dim=1)
         if not self.training:
-            _, action = action.max(1, keepdim=True)
+            _, action = action_prob.max(1, keepdim=True)
             return action, None
 
-        distribution = EGreedy(action, 0.1)
-        action = distribution.sample()
+        # greedy actions
+        if random.random() < 0.1:
+            action = torch.LongTensor(action_prob.size(0), 1).random_(0, action_prob.size(1))
+            action = Variable(action)
+            if USE_CUDA:
+                action = action.cuda()
+        else:
+            _, action = action_prob.max(1, keepdim=True)
 
         # value prediction - critic
         value = F.relu(self.value1(self.hx))
@@ -112,7 +116,8 @@ class AdvantageActorCriticLSTM(BaseModelLSTM):
         value = self.value2(value)
 
         # save output for backpro
-        self.outputs.append(ModelOutput(distribution.log_prob(action), value))
+        action_prob = F.log_softmax(action_prob, dim=1)
+        self.outputs.append(ModelOutput(action_prob.gather(-1, action), value))
         return action, value
 
     def get_action(self, state):
@@ -142,13 +147,15 @@ class AdvantageActorCriticLSTM(BaseModelLSTM):
             returns = returns.cuda()
         #
         # calculate losses
-        loss = 0
-        for i in range(len(self.outputs) - 1):
+        policy_loss = 0
+        value_loss = 0
+        steps = len(self.outputs) - 1
+        for i in range(steps):
             advantage = Variable(returns[i] - self.outputs[i].value.data)
-            loss += (-self.outputs[i].log_action * advantage).sum()
-            loss += F.smooth_l1_loss(self.outputs[i].value, Variable(returns[i]))
-        #
-        # backpro
+            policy_loss += -self.outputs[i].log_action * advantage
+            value_loss += F.smooth_l1_loss(self.outputs[i].value, Variable(returns[i]))
+
+        loss = policy_loss.mean() / steps + value_loss / steps
         loss.backward()
 
         # reset state